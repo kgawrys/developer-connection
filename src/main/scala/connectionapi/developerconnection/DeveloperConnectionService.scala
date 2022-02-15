01cf8aa70600fd7d0f8454f94af9f7877f50dbb3
Krzysztof Gawryś <kgawrys@iterato.rs>
2022-02-15 12:52:08 +0100

    Added parallel processing of requests in developer connection service

--- src/main/scala/connectionapi/developerconnection/DeveloperConnectionService.scala
+++ src/main/scala/connectionapi/developerconnection/DeveloperConnectionService.scala
@@ -3,14 +3,15 @@ package connectionapi.developerconnection
 import cats.data.Validated._
 import cats.data.ValidatedNel
 import cats.effect.Async
+import cats.effect.implicits._
 import cats.implicits.{ catsSyntaxApplicativeError, _ }
 import connectionapi.developerconnection.domain.developerconnection.{ Connected, DeveloperName, OrganizationName }
 import connectionapi.developerconnection.domain.dto.DeveloperConnectionResponse
 import connectionapi.github.GithubService
 import connectionapi.github.domain.dto.GithubOrganization
+import connectionapi.twitter.TwitterService
 import connectionapi.twitter.domain.dto.TwitterUserFollowing
 import org.typelevel.log4cats.Logger
-import connectionapi.twitter.TwitterService
 
 trait DeveloperConnectionService[F[_]] {
   def areConnected(devName1: DeveloperName, devName2: DeveloperName): F[ValidatedNel[Throwable, DeveloperConnectionResponse]]
@@ -23,13 +24,16 @@ object DeveloperConnectionService {
   ): DeveloperConnectionService[F] =
     new DeveloperConnectionService[F] {
 
-      def areConnected(devName1: DeveloperName, devName2: DeveloperName): F[ValidatedNel[Throwable, DeveloperConnectionResponse]] =
-        for {
-          dev1Orgs      <- githubService.getOrganizations(devName1).attempt
-          dev2Orgs      <- githubService.getOrganizations(devName2).attempt
-          dev1Following <- twitterService.followingByDeveloperName(devName1).attempt
-          dev2Following <- twitterService.followingByDeveloperName(devName2).attempt
-        } yield handleResult(devName1, devName2, dev1Orgs, dev2Orgs, dev1Following, dev2Following)
+      def areConnected(devName1: DeveloperName, devName2: DeveloperName): F[ValidatedNel[Throwable, DeveloperConnectionResponse]] = {
+        val dev1Orgs      = githubService.getOrganizations(devName1).attempt
+        val dev2Orgs      = githubService.getOrganizations(devName2).attempt
+        val dev1Following = twitterService.followingByDeveloperName(devName1).attempt
+        val dev2Following = twitterService.followingByDeveloperName(devName2).attempt
+
+        (dev1Orgs, dev2Orgs, dev1Following, dev2Following).parMapN { (dev1Orgs, dev2Orgs, dev1Following, dev2Following) =>
+          handleResult(devName1, devName2, dev1Orgs, dev2Orgs, dev1Following, dev2Following)
+        }
+      }
 
       // This is for accumulative error handling
       private def handleResult(
